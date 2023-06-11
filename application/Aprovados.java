2432955ccf0915f782b5cba8db8adcad9533c28a
Aurea Montero <monteroaurea@gmail.com>
2023-06-11 14:47:24 -0300

    added programs

--- application/Aprovados.java
+++ application/Aprovados.java
@@ -9,13 +9,30 @@ public class Aprovados {
 		
 		
 		int n;
-		String nome;
-		double primeira, segunda;
+		double media;
+		
 		System.out.println("Quantos alunos serão digitados?");
 		n = sc.nextInt();
 		
-		for(int i=0; i<n;i++) {
-			System.out.printf("Digite nome, primeira e segunda nota do %do aluno:" i + 1);
+		String nomes[] = new String[n];
+		double notas1[] = new double[n];
+		double notas2[] = new double[n];
+		
+		
+		for(int i=0; i<n; i++) {
+			System.out.printf("Digite nome, primeira e segunda nota do %do aluno:\n", i + 1);
+			sc.nextLine();
+			nomes[i] = sc.nextLine();
+			notas1[i] = sc.nextDouble();
+			notas2[i] = sc.nextDouble();
+		}
+		System.out.println("Alunos aprovados: ");
+		
+		for (int i=0; i<n; i++) {
+			media = (notas1[i] + notas2[i]) / 2;
+				if (media >= 6.0) {
+					System.out.printf("%s\n", nomes[i]);
+				}
 		}
 		
 		
